9a35e9436f1b8ac9ce1b119ca4691b4ebc3ccfe6
voncho <fencho.jay@gmail.com>
2024-01-12 16:19:42 +0800

    fix: 自动填充

--- backend/src/main/kotlin/com/oz/meow/config/MysqlStyle.kt
+++ backend/src/main/kotlin/com/oz/meow/config/MysqlStyle.kt
@@ -1,12 +1,32 @@
 package com.oz.meow.config
 
+import org.beetl.sql.clazz.SQLType
 import org.beetl.sql.core.ExecuteContext
 import org.beetl.sql.core.SQLExecutor
 import org.beetl.sql.core.db.MySqlStyle
+import java.time.LocalDateTime
 
 class MysqlStyle: MySqlStyle() {
     override fun buildExecutor(executeContext: ExecuteContext?): SQLExecutor {
         val sqlType = executeContext?.sqlSource?.sqlType
+        when (sqlType) {
+            SQLType.INSERT  -> {
+                executeContext.apply {
+                    setContextPara("created", LocalDateTime.now())
+                    setContextPara("creator", 0L)
+                    setContextPara("creatorName", "admin")
+                }
+            }
+            SQLType.UPDATE -> {
+                executeContext.apply {
+                    setContextPara("modified", LocalDateTime.now())
+                    setContextPara("modifier", 0L)
+                    setContextPara("modifierName", "admin")
+                }
+            }
+            SQLType.UNKNOWN -> {}
+            else -> {}
+        }
         return super.buildExecutor(executeContext)
     }
 }
--- domain/src/main/kotlin/com/oz/meow/convert/MetaConvert.kt
+++ domain/src/main/kotlin/com/oz/meow/convert/MetaConvert.kt
@@ -1,15 +1,28 @@
 package com.oz.meow.convert
 
 import org.beetl.sql.annotation.builder.AttributeConvert
+import org.beetl.sql.clazz.SQLType
 import org.beetl.sql.clazz.kit.BeanKit
 import org.beetl.sql.core.ExecuteContext
 
 class MetaConvert: AttributeConvert {
-    override fun toDb(ctx: ExecuteContext?, cls: Class<*>?, name: String?, pojo: Any?): Any {
-        BeanKit.setBeanProperty(pojo, ctx?.getContextPara(name), name)
-        if (ctx != null) {
-            return ctx.getContextPara(name)
+    private val createFields = arrayOf("creator", "created", "creatorName")
+    private val updateFields = arrayOf("modifier", "modified", "modifierName")
+    override fun toDb(ctx: ExecuteContext?, cls: Class<*>?, name: String?, pojo: Any?): Any? {
+        val beanProperty = BeanKit.getBeanProperty(pojo, name)
+        if (beanProperty != null) {
+            return beanProperty
         }
-        return ""
+        val sqlType = ctx?.sqlSource?.sqlType
+        val value = ctx?.getContextPara(name)
+        if (sqlType == SQLType.INSERT && createFields.contains(name)) {
+            BeanKit.setBeanProperty(pojo, value, name)
+            return value
+        }
+        if (sqlType == SQLType.UPDATE && updateFields.contains(name)) {
+            BeanKit.setBeanProperty(pojo, value, name)
+            return value
+        }
+        return null
     }
 }
